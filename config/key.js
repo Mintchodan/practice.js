4f67c24a03a6f38f454ab07a9f2d24f986ba51a5
Mintchodan <z78z@naver.com>
2022-10-28 02:55:13 +0900

    비밀번호 암호화

--- config/key.js
+++ config/key.js
@@ -1,3 +1,5 @@
+// 개발버전, 배포버전에 따라 다른 js파일 
+
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRound = 10 // salt가 몇글자 인지
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,25 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 저장하기 전에 메소드 실행
+userSchema.pre('save', function( next ) {
+    var user = this;
+
+    // 비밀번호가 변경될때만 암호화
+    if(user.isModified('password')) {
+        // salt를 만들어 비밀번호 암호화
+        bcrypt.genSalt(saltRound, function(err, salt) {
+            if(err) return next(err) // salt 만들때 에러시 return
+            bcrypt.hash(user.password, salt, function(err, hash) { // 암호화된 비밀번호는 hash
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } 
+}) 
+
 
 const User = mongoose.model('User', userSchema)
 
