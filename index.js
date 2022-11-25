65d403175896a322a76a240f604c82fd2e1176e6
Mintchodan <z78z@naver.com>
2022-11-26 00:27:23 +0900

    Auth 기능 추가

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { User } = require("./models/User");
 const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 
 const app = express();
 const port = 5000;
@@ -26,7 +27,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -38,7 +39,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -67,6 +68,21 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+// auth가 endpoint의 리퀘스트를 받은다음 콜백폼션 하기 전 중간 기능
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true 라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, // 어드민 유저인지 확인 , role 0 -> 일반유저 , role이 0이 아니면 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
